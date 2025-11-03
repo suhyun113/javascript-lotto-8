5ff174728f4fc8be2f1f94fb6d6eb0cc609bceee
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-11-03 23:06:03 +0900

    feat: App.js 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { askPurchaseAmount, askWinningNumbers, askBonusNumber} from "./io/inputView.js";
+import { printIssuedTickets, printEvaluationResults } from "./io/outView.js";
+import { issueTickets, evaluate } from "./service/lottoService.js";
+
+
 class App {
-  async run() {}
+  async run() {
+    const amount = await askPurchaseAmount();
+    const tickets = issueTickets(amount);
+    printIssuedTickets(tickets);
+
+    const winningNumbers = await askWinningNumbers();
+    const bonusNumber = await askBonusNumber(winningNumbers);
+
+    const { counts, totalPrize } = evaluate(tickets, winningNumbers, bonusNumber);
+    printEvaluationResults(counts, totalPrize, amount);
+  }
 }
 
 export default App;
